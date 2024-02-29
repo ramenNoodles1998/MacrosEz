20e965c0f67762dec9b4416c1312ca274151b6a2
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2024-02-28 20:07:35 -0700

    setup logs

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 	"strconv"
 )
 
+type Log struct{
+	Protein float64 
+	Carbs float64
+	Fat float64
+}
+
 func main() {
 	fmt.Println("hello world")
+	var logs [0]Log
 
 	var temp *template.Template = template.Must(template.ParseGlob("./template/*.html"))
-	var data = map[string]float64{
+	var data = map[string]interface{}{
 		"Protein": 0.0,
 		"Carbs": 0.0,
 		"Fat": 0.0,
@@ -19,7 +26,11 @@ func main() {
 		"ProteinValue": 0.0,
 		"CarbsValue": 0.0,
 		"FatValue": 0.0,
+		"Logs": logs,
 	}
+	//TODO: add add all button
+	//h1 logs remove if no logs
+	//add saving logs and deleting.
 		
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -33,18 +44,22 @@ func main() {
 		carbs, _ := strconv.ParseFloat(r.Form["carbs"][0], 64)
 		fat, _ := strconv.ParseFloat(r.Form["fat"][0], 64)
 		//TODO: save log here
-
-		data = map[string]float64{
-			"Protein": data["Protein"] + protein,
-			"Carbs": data["Carbs"] + carbs,
-			"Fat": data["Fat"] + fat,
-			"Calories": data["Calories"] + (protein * 4) + (carbs * 4) + (fat * 9),
+		data = map[string]interface{}{
+			"Protein": data["Protein"].(float64) + protein,
+			"Carbs": data["Carbs"].(float64) + carbs,
+			"Fat": data["Fat"].(float64) + fat,
+			"Calories": data["Calories"].(float64) + (protein * 4) + (carbs * 4) + (fat * 9),
 			"ProteinValue": 0.0,
 			"CarbsValue": 0.0,
 			"FatValue": 0.0,
+			"Logs": getLogs(),
 		}
 		temp.Execute(w, data)
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func getLogs() []Log {
+	return []Log {{ Protein: 0, Carbs: 0, Fat: 0,}}
 }
