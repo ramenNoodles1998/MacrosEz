cec25be33839fde8849c7b753c053d9951c9573f
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2024-02-29 23:17:44 -0700

    add creating food items.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	// "github.com/google/uuid"
@@ -24,6 +25,17 @@ type Log struct{
 	Fat float64
 }
 
+type FoodItem struct{
+	//uuid
+	PartitionKey string
+	//name
+	SortKey string
+	Protein float64 
+	Carbs float64
+	Fat float64
+}
+
+const nameSuffix = "NAME-"
 const tableName string = "dev-macros"
 const (
     YYYYMMDD = "2006-01-02"
@@ -37,6 +49,7 @@ var data = map[string]interface{}{
 	"Fat": 0.0,
 	"Calories": 0.0,
 	"Logs": []Log{},
+	"FoodItems": []FoodItem{},
 }
 
 func main() {
@@ -48,8 +61,6 @@ func main() {
 	svc := dynamodb.New(sess)
 	
 	initData(svc)
-
-	//do this for calories and macros.
 
 	var temp *template.Template = template.Must(template.ParseGlob("./template/*.html"))
 	//TODO: add add all button
@@ -75,12 +86,77 @@ func main() {
 			"Fat": data["Fat"].(float64) + fat,
 			"Calories": data["Calories"].(float64) + (protein * 4) + (carbs * 4) + (fat * 9),
 			"Logs": getLogs(svc),
+			"FoodItems": getFoodItems(svc),
 		}
 
 		temp.ExecuteTemplate(w, "index.html", data)
 	})
 
+	http.HandleFunc("/save-food-item", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		name := r.Form["name"][0]
+		protein, _ := strconv.ParseFloat(r.Form["protein"][0], 64)
+		carbs, _ := strconv.ParseFloat(r.Form["carbs"][0], 64)
+		fat, _ := strconv.ParseFloat(r.Form["fat"][0], 64)
+
+		saveFoodItem(svc, name, protein, carbs, fat)
+
+		data["FoodItems"] = getFoodItems(svc)
+
+		temp.ExecuteTemplate(w, "food-items-list.html", data)
+	})
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func getFoodItems(svc *dynamodb.DynamoDB) []FoodItem {
+	//get todays logs.
+	result, err := svc.Query(&dynamodb.QueryInput{
+		TableName: aws.String(tableName),
+		KeyConditions: map[string]*dynamodb.Condition{
+			"PartitionKey": {
+				ComparisonOperator: aws.String("EQ"),
+				AttributeValueList: []*dynamodb.AttributeValue {
+					{
+						S: aws.String(uuidRoman),
+					},
+				},
+			},
+			"SortKey": {
+				ComparisonOperator: aws.String("BEGINS_WITH"),
+				AttributeValueList: []*dynamodb.AttributeValue {
+					{
+						S: aws.String("NAME"),
+					},
+				},
+			},
+			
+		},
+	})
+
+	if err != nil {
+		fmt.Printf("Got error calling GetItem: %s", err)
+		return []FoodItem{}
+	}
+
+	if result.Items == nil {
+		fmt.Printf("Could not find Logs")
+		return []FoodItem{}
+	}
+    
+	foodItems := []FoodItem{}
+
+	for _, item := range result.Items {
+		fi := FoodItem{}
+		err = dynamodbattribute.UnmarshalMap(item, &fi)
+		fi.SortKey, _ = strings.CutPrefix(fi.SortKey, nameSuffix)
+		foodItems = append(foodItems, fi)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		}
+	}
+
+	return foodItems
 }
 
 func initData(svc *dynamodb.DynamoDB) {
@@ -100,6 +176,36 @@ func initData(svc *dynamodb.DynamoDB) {
 	data["Fat"] = fat 
 	data["Calories"] = (protein * 4) + (carbs * 4) + (fat * 9)
 	data["Logs"] = logs 
+	data["FoodItems"] = getFoodItems(svc)
+}
+
+func saveFoodItem(svc *dynamodb.DynamoDB, name string, protein float64, carbs float64, fat float64){
+	foodItem := FoodItem{
+		PartitionKey: uuidRoman,
+		SortKey: nameSuffix + name,
+		Protein: protein,
+		Carbs: carbs,
+		Fat: fat,
+	}
+
+	av, err := dynamodbattribute.MarshalMap(foodItem)
+	if err != nil {
+		fmt.Printf("Got error marshalling new movie item: %s", err)
+		return
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item: av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+
+	if err != nil {
+		fmt.Printf("Got error calling PutItem: %s", err)
+		return
+	}
+
 }
 
 func saveLog(svc *dynamodb.DynamoDB, protein float64, carbs float64, fat float64) {
@@ -147,7 +253,7 @@ func getLogs(svc *dynamodb.DynamoDB) []Log {
 				ComparisonOperator: aws.String("BEGINS_WITH"),
 				AttributeValueList: []*dynamodb.AttributeValue {
 					{
-						S: aws.String(time.Now().UTC().Format(YYYYMMDD)),
+						S: aws.String(time.Now().Format(YYYYMMDD)),
 					},
 				},
 			},
